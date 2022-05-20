2a5854aecfd669fae417c3419530776da471e9b4
Ernie Hsiung <e@erniehsiung.com>
2022-05-20 19:31:54 -0400

    progress so far

---
+++ src/components/FooterArea/FooterArea.test.tsx
@@ -0,0 +1,11 @@
+import {render, screen, fireEvent} from '@testing-library/react';
+import FooterArea from './FooterArea';
+
+// render the component
+// click on the "Clear completed" button
+// verify the completed items are cleared
+
+// render the component with some active and completed items
+// click on the Active filter link
+// verify the Active link is active
+// verify only the active items are shown
